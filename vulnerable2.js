d154192d3967faaf7f4b401e7d93d42ee82db7eb
harshal patel <hp842484@gmail.com>
2026-06-02 00:21:54 +0530

    Test bot with Javascript SHA-1

---
+++ vulnerable2.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+function insecureLegacyFunction() {
+    // Spectra should catch this SHA-1 usage!
+    const shasum = crypto.createHash('sha1');
+    shasum.update('hunter2');
+    return shasum.digest('hex');
+}
