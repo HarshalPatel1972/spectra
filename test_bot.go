e4ce18495aff8a91328996088fb5018eddfbf644
harshal patel <hp842484@gmail.com>
2026-06-02 00:17:19 +0530

    Fix go build package mismatch

--- test_bot.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"crypto/rsa"
-)
-
-func myVulnerableFunction() {
-	_, _ = rsa.GenerateKey(nil, 2048)
-}
--- test_bot_3.go
+++
@@ -1,9 +0,0 @@
-package dummy
-
-import (
-	"crypto/md5"
-)
-
-func anotherVulnerableFunction() {
-	_ = md5.New()
-}
